9f42a435597e2fe11b25b29d270dfbebbfb36818
Azafa Dev <akmal.dz.f@gmail.com>
2026-02-22 00:38:34 +0700

    sanity

---
+++ company-profile-next/src/sanity/schemaTypes/post.ts
@@ -0,0 +1,79 @@
+import {defineField, defineType} from 'sanity'
+
+export const post = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: {type: 'author'}, // Ini yang tadi error karena author belum ada
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Process', value: 'Process'},
+          {title: 'Origin', value: 'Origin'},
+          {title: 'Brewing', value: 'Brewing'},
+          {title: 'Lifestyle', value: 'Lifestyle'},
+        ],
+      },
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'readTime',
+      title: 'Reading Time (e.g. 5 min read)',
+      type: 'string',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent', // Ini yang tadi error karena blockContent belum ada
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+})
