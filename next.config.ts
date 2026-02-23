412ee4c7d316f1ea1140e83f8e146859a5a89e38
Azafa Dev <akmal.dz.f@gmail.com>
2026-02-23 12:00:38 +0700

    adding protocol

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "janji-jiwa-coffee-company-profile.vercel.app",
+      }
     ],
   },
 };
